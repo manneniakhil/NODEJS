8103b5cdebd8fcbc2205a0fa9132415d50285308
manneniakhil <46835947+manneniakhil@users.noreply.github.com>
2024-04-08 15:52:53 -0400

    Delete userEvents.mjs

--- userEvents.mjs
+++
@@ -1,9 +0,0 @@
-//const EventEmitter =require('events')
-import * as Events from 'events';
-
- export class UserEvents extends Events.EventEmitter{
-    createPost(content){
-        console.log('Post is created');
-        this.emit('postCreated')
-    }
-}
