1b134c70358a7ecc58f759008c4e31539136fb9a
manneniakhil <46835947+manneniakhil@users.noreply.github.com>
2024-04-08 14:00:07 -0400

    Add files via upload
    In this the sender email and recover email address are taken from terminal using readline

--- sendingmailnode.js
+++ sendingmailnode.js
@@ -1,25 +1,39 @@
 //1. import nodemailer
 
 const nodemailer=require('nodemailer');
+const readline=require('readline');
+//configure interface to connect with terminal
+const k=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+//Function to prompt for user input
+async function prompt(question){
+    return new Promise((resolve)=>{
+        k.question(question,(input)=>resolve(input));
+    });
+
+}
 
 //2.configure and send it
 
-async function sendMail(){
+async function sendMail(senderEmail,senderPassword,recieverEmail){
 
     //1. Create an email transporter
     //SMTP-simple mail transport protocol
     const transporter = nodemailer.createTransport({
         service:'gmail',
         auth: {
-            user:'akhilmanneni@gmail.com',
-            pass:'wtlo kxoh onog hwsi',
+            user:senderEmail,
+            pass:senderPassword,
         },
     });
 
    // 2. Configure mail content
    const mailOptions={
-    from:'akhilmanneni@gmail.com',
-    to:'manneniakhil@gmail.com',
+    from:senderEmail,
+    to:recieverEmail,
     subject: 'Welocome to Nodejs App',
     text:'This is a email using nodemailer in Node.js',
    };
@@ -35,4 +49,13 @@ async function sendMail(){
 
 }
 
-sendMail();
+//Main function to execute the flow
+async function main(){
+    const senderEmail= await prompt("Enter sender email: ");
+    const senderPassword= await prompt("Enter password: ");
+    const recieverEmail=await prompt("Enter reciever Email: ")
+
+    k.close();
+    await sendMail(senderEmail,senderPassword,recieverEmail);
+}
+main().catch(console.error);
